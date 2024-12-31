139a28d9941129a2eb6bf570fb20fca8464e1ace
imran-binhasan <imranbinhasan.work@gmail.com>
2025-01-01 02:18:42 +0600

    Finish basic setup

---
+++ Enviroment Setup/test.js
@@ -0,0 +1,5 @@
+"use strict";
+const a = 2;
+console.log(a);
+const b = 'Hello world';
+console.log(b);
---
+++ Enviroment Setup/test.ts
@@ -0,0 +1,5 @@
+const a:number = 2;
+console.log(a)
+
+const b:string = 'Hello world'
+console.log(b)
